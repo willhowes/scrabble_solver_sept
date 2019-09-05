359759e4ae04aa4d18fbaf8d44b62ac6a6a4aa20
Will Howes <williameldenhowes@gmail.com>
2019-09-05 07:51:50 +0100

    tidied up

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,9 @@
 class Scrabble
 
-
   def initialize(letters)
     @letters = letters.upcase
-    @score_matrix = {
 
+    @score_matrix = {
       'A'  => 1, 'E'  => 1, 'I'  => 1, 'O'  => 1, 'U'  => 1, 'L'  => 1, 'N'  => 1, 'R'  => 1, 'S' => 1, 'T' => 1,
       'D' =>	2, 'G' =>	2,
       'B' =>	3, 'C' =>	3, 'M' =>	3, 'P' =>	3,
